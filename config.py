bfe75f100dbd2574c119093cfbaade4970db1119
Zenonia <theinhtooaung906@gmail.com>
2026-02-11 10:37:49 +0630

    minor requirements.txt changes

--- config.py
+++ config.py
@@ -5,9 +5,6 @@ load_dotenv()
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-WEBHOOK_PATH = "/webhook"
-PORT = int(os.environ.get("PORT", 8080))
-
 INPUT_DIR = "input_pdfs"
 OUTPUT_DIR = "output_pdfs"
 HEADER_IMAGE = "images/header.jpg"
